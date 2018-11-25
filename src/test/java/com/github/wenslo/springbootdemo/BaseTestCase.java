19edc16fce152be2e99aa62a2067a738fe2507ef
wenslo <wenhlo@163.com>
2018-11-25 12:52:56 +0800

    mulit profile commit

---
+++ src/test/java/com/github/wenslo/springbootdemo/BaseTestCase.java
@@ -0,0 +1,54 @@
+package com.github.wenslo.springbootdemo;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import org.dbunit.DataSourceBasedDBTestCase;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+
+import javax.sql.DataSource;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+/**
+ * @author wenhailin
+ * @version 0.0.1
+ * @createTime 2018年11月25日 下午12:45
+ * @description 基础测试基类
+ */
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class})
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringBootDemoApplication.class)
+@ActiveProfiles("test")
+public abstract class BaseTestCase extends DataSourceBasedDBTestCase {
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected DataSource getDataSource() {
+        return dataSource;
+    }
+
+    @Override
+    protected IDataSet getDataSet() throws Exception {
+        InputStream inputStream = getClass().getResourceAsStream("/dataset.xml");
+        Reader reader = new InputStreamReader(inputStream);
+        return new FlatXmlDataSetBuilder().build(reader);
+    }
+}
--- src/test/java/com/github/wenslo/springbootdemo/repository/UserRepositoryTest.java
+++ src/test/java/com/github/wenslo/springbootdemo/repository/UserRepositoryTest.java
@@ -1,29 +1,12 @@
 package com.github.wenslo.springbootdemo.repository;
 
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
-import com.github.wenslo.springbootdemo.SpringBootDemoApplication;
+import com.github.wenslo.springbootdemo.BaseTestCase;
 import com.github.wenslo.springbootdemo.model.User;
 import com.github.wenslo.springbootdemo.reposiroty.UserRepository;
-import org.dbunit.DataSourceBasedDBTestCase;
-import org.dbunit.dataset.IDataSet;
-import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
-import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
-import javax.sql.DataSource;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
 import java.util.List;
 
 /**
@@ -32,35 +15,17 @@ import java.util.List;
  * @createTime 2018年11月24日 下午6:11
  * @description
  */
-@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
-        DirtiesContextTestExecutionListener.class,
-        TransactionalTestExecutionListener.class,
-        DbUnitTestExecutionListener.class })
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = SpringBootDemoApplication.class)
-public class UserRepositoryTest extends  DataSourceBasedDBTestCase {
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
-    @Autowired
-    private DataSource dataSource;
+
+public class UserRepositoryTest extends BaseTestCase {
+
     @Autowired
     private UserRepository userRepository;
 
     @Test
     public void testFindAll() {
         List<User> list = userRepository.findAll();
-        logger.info("list size is {}",list.size());
+        logger.info("list size is {}", list.size());
         Assert.assertTrue(!list.isEmpty());
     }
 
-    @Override
-    protected DataSource getDataSource() {
-        return dataSource;
-    }
-
-    @Override
-    protected IDataSet getDataSet() throws Exception {
-        InputStream inputStream = getClass().getResourceAsStream("/dataset.xml");
-        Reader reader = new InputStreamReader(inputStream);
-        return new FlatXmlDataSetBuilder().build(reader);
-    }
 }
