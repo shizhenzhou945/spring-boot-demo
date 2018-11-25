501922468be4d99d6aeade04eb72b6cea97694ce
wenhailin <wenhlo@163.com>
2018-11-25 17:45:56 +0800

    test

--- src/main/java/com/github/wenslo/springbootdemo/config/WebSecurityConfig.java
+++ src/main/java/com/github/wenslo/springbootdemo/config/WebSecurityConfig.java
@@ -1,11 +1,12 @@
 package com.github.wenslo.springbootdemo.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author wenhailin
@@ -14,11 +15,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @description
  */
 @EnableWebSecurity
-public class WebSecurityConfig implements WebMvcConfigurer {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
     @Bean
-    public UserDetailsService userDetailsService() throws Exception {
+    public UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
+        manager.createUser(User.withDefaultPasswordEncoder().username("user1").password("1234").roles("SUPER_MAN").build());
+        manager.createUser(User.withDefaultPasswordEncoder().username("user2").password("1234").roles("ADMIN").build());
         return manager;
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/test/**").access("hasRole('ADMIN') and hasRole('DBA')")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin();
+        ;
     }
 }
