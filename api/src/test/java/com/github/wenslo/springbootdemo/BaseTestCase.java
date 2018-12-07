d2464e8699c8ed722575d3e829214503d105f774
wenhailin <4341wen!>
2018-12-07 19:22:55 +0800

    controller test

---
+++ api/src/test/java/com/github/wenslo/springbootdemo/BaseTestCase.java
@@ -0,0 +1,40 @@
+package com.github.wenslo.springbootdemo;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
+
+/**
+ * @author wenhailin
+ * @version 0.0.1
+ * @createTime 2018年11月25日 下午12:45
+ * @description 基础测试基类
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringBootDemoApplication.class,webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public abstract class BaseTestCase  {
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private WebApplicationContext context;
+
+    protected MockMvc mvc;
+
+    @Before
+    public void setup(){
+        mvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+    }
+
+}
