7a55bde7844520d21c35cc425dce60a764b8ff19
wenslo <wenhlo@163.com>
2019-01-26 23:06:27 +0800

    update mongodb config

--- core/src/main/java/com/github/wenslo/springbootdemo/MongoConfig.java
+++ core/src/main/java/com/github/wenslo/springbootdemo/MongoConfig.java
@@ -1,13 +1,14 @@
 package com.github.wenslo.springbootdemo;
 
 import com.mongodb.MongoClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
-import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
-import org.springframework.data.mongodb.core.convert.MongoTypeMapper;
+import org.springframework.core.env.Environment;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import java.util.Objects;
 
 /**
  * @author wenhailin
@@ -17,31 +18,22 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
  */
 @EnableMongoRepositories({"com.github.wenslo.springbootdemo.reposiroty"})
 @Configuration
-public class MongoConfig extends AbstractMongoConfiguration {
-    @Override
-    protected String getDatabaseName() {
-        return "database";
-    }
-
-    @Override
-    public MongoClient mongoClient() {
-        return new MongoClient("192.168.1.221");
-    }
+public class MongoConfig {
+    private static final String MONGO_DATABASE_KEY = "spring.data.mongodb.database";
+    private static final String MONGO_HOST_KEY = "spring.data.mongodb.host";
+    private static final String MONGO_PORT_KEY = "spring.data.mongodb.port";
+    private static final String DEFAULT_HOST = "127.0.0.1";
+    private static final Integer DEFAULT_PORT = 27017;
+    @Autowired
+    private Environment environment;
 
     @Bean
-    @Override
-    public MappingMongoConverter mappingMongoConverter() throws Exception {
-        MappingMongoConverter mmc = super.mappingMongoConverter();
-        mmc.setTypeMapper(customTypeMapper());
-        return mmc;
-    }
-
-    @Bean
-    public MongoTypeMapper customTypeMapper() {
-        return new CustomMongoTypeMapper();
-    }
-
-    private class CustomMongoTypeMapper extends DefaultMongoTypeMapper {
-        //implement custom type mapping here
+    public MongoTemplate mongoTemplate() {
+        String host = environment.getProperty(MONGO_HOST_KEY);
+        host = Objects.isNull(host) ? DEFAULT_HOST : host;
+        String portStr = environment.getProperty(MONGO_PORT_KEY);
+        int port = Objects.isNull(portStr) ? DEFAULT_PORT : Integer.parseInt(portStr);
+        MongoClient mongoClient = new MongoClient(host, port);
+        return new MongoTemplate(mongoClient, Objects.requireNonNull(environment.getProperty(MONGO_DATABASE_KEY)));
     }
 }
