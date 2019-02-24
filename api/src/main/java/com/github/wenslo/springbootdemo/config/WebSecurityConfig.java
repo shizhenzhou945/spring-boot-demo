05e6f2a2e9d72344008a97df7d771051155a8a11
wenslo <wenhlo@163.com>
2019-02-24 20:24:54 +0800

    restore export permission

--- api/src/main/java/com/github/wenslo/springbootdemo/config/WebSecurityConfig.java
+++ api/src/main/java/com/github/wenslo/springbootdemo/config/WebSecurityConfig.java
@@ -40,7 +40,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .cors()
                 .and()
                 .antMatcher("/**").authorizeRequests()
-                .antMatchers("/login**", "/user/export").permitAll()
+                .antMatchers("/login**").permitAll()
                 .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                 .anyRequest().authenticated()
                 .and()
